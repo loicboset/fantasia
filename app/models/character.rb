fb9afc7a2c432fc64d052e13bcea4a5ea6ab0af3
Loic Boset <loic.boset@gmail.com>
2020-08-17 15:08:06 +0200

    Adding characters controller and routes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,8 @@
 class Character < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :review, through: :bookings
+
   validates :name, :description, :price_per_day, :image_url, presence: true
   validates :name, uniqueness: true
   validates :price_per_day, numericality: { only_integer: true }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'characters/index'
+  get 'characters/show'
   devise_for :users
   root to: 'pages#home'
-  resources :characters
+  resources :characters, except: [:destroy] do
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
