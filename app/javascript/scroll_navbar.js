1ea209eebd20bff93c976581d24752901dc0ab1b
Loic Boset <loic.boset@gmail.com>
2020-08-18 17:11:24 +0200

    Adding search bar on homepage

--- app/javascript/scroll_navbar.js
+++ app/javascript/scroll_navbar.js
@@ -5,12 +5,14 @@ console.log("hello from scroll_navbar.js");
 const scroll_navbar = () => {
   window.addEventListener('scroll', (e) => {
     const navbar = document.querySelector('.navbar-lewagon');
+    const searchBar = document.querySelector('.simple_form.search-bar');
     const navbarPos = document.querySelector('.navbar-lewagon').offsetTop;
-    console.log(navbarPos)
     if (window.pageYOffset > navbarPos) {
       navbar.classList.add("sticky")
+      searchBar.classList.add("sticky-bar")
     } else {
       navbar.classList.remove("sticky");
+      searchBar.classList.remove("sticky-bar")
     }
   })
 };
